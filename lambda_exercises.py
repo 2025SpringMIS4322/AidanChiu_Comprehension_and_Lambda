6138855e87b7ae0278da3d22cfd55e253263302a
Aidan Chiu <aidan_chiu1@baylor.edu>
2025-04-05 00:06:31 -0500

    Lambda and List Comprehension hw

--- lambda_exercises.py
+++ lambda_exercises.py
@@ -7,6 +7,11 @@ odd_numbers = [1, 3, 5, 7, 9]
 '''
 original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+print()
+even_numbers = list(filter(lambda num: (num % 2 == 0), original_list))
+print(f"even_numbers = {even_numbers}")
+odd_numbers = list(filter(lambda num: (num % 2 != 0), original_list))
+print(f"odd_numbers = {odd_numbers}\n")
 
 
 
@@ -17,8 +22,8 @@ Create a list of days that have exactly 6 characters using a lambda function on 
 weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 
-
-
+six_char_list = list(filter(lambda x: len(x) == 6, weekdays))
+print(f"{six_char_list}\n")
 
 
 
@@ -35,6 +40,10 @@ Expected output
 
 '''
 
+og_list = ['orange', 'red', 'green', 'blue', 'white', 'black']
+remove_from_list = {'orange', 'black'} #TUPLE
+reduced_list = list(filter(lambda word: word not in remove_from_list, og_list)) 
+print(f"{reduced_list}\n")
 
 
 ''' 4) 
@@ -46,10 +55,11 @@ Expected output: [32.0, 53.6, 93.2, 77.0, 23.0]
 
 cel = [0, 12, 34, 25, -5]
 
+fah = list(map(lambda celsius: (celsius * 9/5) + 32, cel)) # map() fct --> applies given fct to every item in an iterable (like a list or tuple)
+print(f"{fah}\n")                                          # and returns a map() object (typically converted to a list or another iterable type)
 
 
-
-''' 5) Use map() with a lambda function to calculate the final price for each item in the card after applying a 10% discount.
+''' 5) Use map() with a lambda function to calculate the final price for each item in the cart after applying a 10% discount.
 '''
 
 cart = [
@@ -59,6 +69,10 @@ cart = [
     {'name': 'Monitor', 'price': 150}
 ]
 
+disc_applied = list(map(lambda item: {'name': item['name'], 'price': round(item['price'] * 0.90, 2)}, cart))
+for item in disc_applied:
+    print(f"{item['name']} discounted price = ${item['price']:.2f}")
+print()
 
 
 
@@ -72,11 +86,12 @@ Write a lambda function that takes two arguments x and y and returns:
 
  '''
 
-compare = 
+compare = lambda x,y: 'x is greater' if x > y else 'y is greater' if y > x else 'Equal'
 
 print(compare(5, 3))  # Output: 'x is greater'
 print(compare(3, 7))  # Output: 'y is greater'
 print(compare(4, 4))  # Output: 'Equal'
+print()
 
 
 
@@ -89,7 +104,8 @@ Expected Output: ['C', 'Java', 'Ruby', 'Python', 'JavaScript']
 '''
 languages = ['Python', 'Java', 'C', 'Ruby', 'JavaScript']
 
-
+ascending_lang = sorted(languages, key=lambda asc: len(asc))
+print(f"{ascending_lang}\n\n")
 
 
 
@@ -104,7 +120,9 @@ original_scores = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sci
 # [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
 '''
 
-
+original_scores = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+new_tuples = sorted(original_scores, key=lambda x: x[1]) #This structure of Tuples sorts the OG scores list by the 'x' key, which lambda acts on
+print(f"{new_tuples}\n\n")                                 #and sorts it ascending by x[1], which refers to tuple's number element at index 1
 
 
 
@@ -117,14 +135,8 @@ curved scores = {'Alice': 90, 'Bob': 92, 'Charlie': 83}
 '''
 
 students = {'Alice': 85, 'Bob': 92, 'Charlie': 78}
-curved_scores = 
-
-
-
-
-
-
-
+curved_scores = {student: (lambda score: score + 5)(score) for student, score in students.items()}
+print(f"{curved_scores}\n")
 
 
 
--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -25,20 +25,83 @@ for i in original_list:
 #result is going to be a list.
 
 #There are 3 parts to list comprehension:
-
-#*result*  = [*transform/expression*    *iteration*         *filter*     ]
+#*result*  = [*transform/expression*    *iteration*         *filter*/*condition*     ]         You don't necessarily need commas between the items. Just a space is fine.
+#                                       #for x in list_1          if x> 7
 
 #The filter part answers the question if the item should be transformed.
 
+# Order of list sytax execution****
+#ITERATION FIRST
+#CONDITION SECOND
+#EXPRESSION LAST
+
+#Create a list of numbers between 1 and 10 using LIST COMPREHENSION
+x = [i for i in range(1,11)]
+print(x)
+
+#add an expression to the above 
+squares = [i**2 for i in range(1,11)]
+print(squares)
+
+list1 = [3,4,5]
+multiplied = [item*3 for item in list1] #Iterator is ITEM --> naming MUST be consistent
+print(multiplied)
+
+#Working with strings
+listofwords = ['this', 'is', 'a', 'list', 'of', 'words']
+output = [word[0].upper() for word in listofwords]  #APPLY INDEX VALUES FOR getting the SPECIFIC LETTERS OF WORDS.  APPLY .upper() in Transform/Expression
+print(output)
+
+# Add in an IF condition.             ****Iteration is MANDATORY. Expression and Condition OPTIONAL****
+# Find the square of all even numbers from 1-20
+result = [i*i for i in range(1,21) if i % 2 == 0]  # i % 2 checks remainders of numbers. If remainder = 0, number is even, and you know the rest
+print(result)
 
 
-## Exercise ##
+# Practice List comprehension
+string = 'Hello 12345 World'
+# Use list Comp to EXTRACT THE NUMBERS ONLY - [1,2,3,4,5]
+numbers = [int(num) for num in string if num.isdigit()]  # Convert item types in EXPRESSION. 
+print(numbers)
+# Extract letters only - ['h', 'e']
+letters = [str(letter) for letter in string if letter.isalpha()]  
+print(letters)
+# Extract whole words only - ['Hello', 'World']
+words = [word for word in string.split() if word.isalpha()]   # Use SPLIT function in ITERATION. Iteration splits up string first, then condition applies.
+print(words)
+
+
+# Look for and extract text from a file
+infile = open('test.txt', 'r')
+result = [i.rstrip() for i in infile if 'line3' in i] #Directly apply infile. 
+print(result) 
+
+
+# We can use list comp with a function
+def double(x): 
+    return x*2
+
+result = [double(x) for x in range(1,11) if x % 2 == 0] 
+print(result)
+
+
+# Work with multiple iterables 
+result = [(x,y) for x in [10,30,50] for y in [20,40,60] if x+y > 70] # (x,y) is tuple
+print(f"{result}\n")
+
+
+
+
+## Exercise ##                PART OF HW ASSIGNMENT
 
 # 1 Using a list comprehension, create a new list called "newlist" out of the list "numbers", 
 # which contains only the positive numbers from the list, as integers.
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
+print('Exercises - HW')
+newlist = [int(num) for num in numbers if num > 0]
+print(f"{newlist}\n")
 
 
 
@@ -48,6 +111,8 @@ numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
 
+word_lengths = [len(word) for word in words if word != 'the']
+print(f"{word_lengths}\n")
 
 
 ## Given dictionary is consisted of vehicles and their weights in kilograms. 
@@ -57,24 +122,33 @@ words = sentence.split()
 dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
 "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
+vehicles = {"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
+"Semi": 13600, "Bicycle": 7, "Motorcycle": 110} # I changed the name of the dictionary to prevent TypeErrors
 
+light_vehicles = [vehicle.upper() for vehicle, weight in vehicles.items() if weight < 5000]
+print(f"{light_vehicles}\n")
 
 
 ## Find all the numbers from 1 to 1000 that have a 4 in them
-
+num_with_4 = [num for num in range(1,1001) if '4' in str(num)]
+print(f"{num_with_4}\n")
 
 
 ## count how many times the word 'the' appears in the text file - 'sometext.txt'
+wanted_word = 'the' #define target word
+infile2 = open('sometext.txt', 'r') # open and read text file
+words = infile2.read().split() #split file content into words
+wanted_word_count = sum([1 for word in words if word.lower() == wanted_word.lower()]) # Count occurrences of a target word
+print(f"The word {wanted_word} appears {wanted_word_count} times in the sometext.txt file\n")
 
 
 
 ## Extract the numbers from the following phrase ##
 
-phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " +
-"event, with about 3 or 4 that were classifled as serious per event.'
+phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each event, with about 3 or 4 that were classifled as serious per event.'
 
-
-
+extract_num = [int(word) for word in phrase.split() if word.isdigit()] 
+print(f"{extract_num}\n")
 
 
 
